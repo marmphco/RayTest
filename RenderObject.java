d2259920a1740bc416637add948dcaf922229c71
Matthew Jee <marmphbob@gmail.com>
2011-10-03 18:00:41 -0700

    some testing that doesn't change functionality

--- RenderObject.java
+++ RenderObject.java
@@ -4,5 +4,5 @@ public interface RenderObject
     public Vector3 intersectionWithRay(Vector3 ray);
     public Vector3 normalAtPoint(Vector3 point);
    // public Vector3 colorAtPoint(); materialAtPoint(); ?
-
+//ololololool test
 }
