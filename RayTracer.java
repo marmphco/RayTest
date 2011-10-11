6c8dd38632cdda72521ea8e7be8157ba0b1215ec
Mathhew Jee <mcjee@slugmail.ucsc.edu>
2011-10-11 09:54:40 -0700

    removed useless imports

--- RayTracer.java
+++ RayTracer.java
@@ -1,5 +1,4 @@
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
 
 public class RayTracer implements Renderer
 {
--- Renderer.java
+++ Renderer.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public interface Renderer {
 
     public void renderImage();
